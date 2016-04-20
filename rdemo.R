ed30a521d529beae9858b64e5c7d5b8403534f25
Douglas Ashton <douglas.j.ashton@gmail.com>
2016-04-20 10:02:16 +0100

    Reproducible research

--- rdemo.R
+++ rdemo.R
@@ -2,3 +2,11 @@
 # Network Plot ------------------------------------------------------------
 
 
+# Clustering --------------------------------------------------------------
+
+
+kc <- kmeans(iris[,-5], 3)
+
+plot(iris[c("Sepal.Length", "Sepal.Width")], pch=16, col=kc$cluster)
+points(kc$centers[,c("Sepal.Length", "Sepal.Width")], col=1:3, pch=8, cex=2)
+
